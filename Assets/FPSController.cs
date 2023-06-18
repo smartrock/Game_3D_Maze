c7d2d9182a271bbefd5c9ea4c72935e6867094bb
smartrock <107009802+smartrock@users.noreply.github.com>
2023-06-18 15:01:44 +1200

    Camera Movement
    This allows the player to look around as well as jump

--- Assets/FPSController.cs
+++ Assets/FPSController.cs
@@ -6,12 +6,19 @@ using UnityEngine;
 
 public class FPSController : MonoBehaviour
 {
+    public Camera playerCamera;
     public float walkSpeed = 6f;
     public float runSpeed = 12f;
+    public float jumpPower = 12f;
+    public float gravity = 20f;
+
+    public float lookSpeed = 5f;
+    public float lookXLimit = 45f;
 
     Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
 
-    bool canMove = true;
+    public bool canMove = true;
 
     CharacterController characterController;
 
@@ -19,6 +26,8 @@ public class FPSController : MonoBehaviour
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -33,6 +42,28 @@ public class FPSController : MonoBehaviour
         float movementDirectionY = moveDirection.y;
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        {
+            moveDirection.y = jumpPower;
+        }
+        else
+        {
+            moveDirection.y = movementDirectionY;
+        }
+
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
         characterController.Move(moveDirection * Time.deltaTime);
+
+        if (canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
     }
 }
